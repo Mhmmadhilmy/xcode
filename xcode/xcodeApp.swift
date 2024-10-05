6228c6c8b1fe154a26f5fdc3813c628293f9a9d5
mhmmadhilmy <mohammadhilmy17@gmai.com>
2024-10-05 21:01:38 +0700

    Initial Commit

---
+++ xcode/xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeApp.swift
+//  xcode
+//
+//  Created by Muhammad Hilmy on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcodeTests/xcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeTests.swift
+//  xcodeTests
+//
+//  Created by Muhammad Hilmy on 05/10/24.
+//
+
+import Testing
+@testable import xcode
+
+struct xcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
